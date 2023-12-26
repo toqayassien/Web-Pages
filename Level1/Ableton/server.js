43657a40fbec1f9764162f87dd49e8482c59d159
toqayassien <122563592+toqayassien@users.noreply.github.com>
2023-12-26 18:38:01 +0200

    Testing first commit

---
+++ Level1/Ableton/server.js
@@ -0,0 +1,25 @@
+// const express = require('express');
+// const cors = require('cors'); // Install using npm install cors
+// const axios = require('axios'); // Install using npm install axios
+
+// const app = express();
+// const port = 3000;
+
+// app.use(cors());
+
+// app.get('/api/data', async (req, res) => {
+//   try {
+//     // Make a request to the external API
+//     const response = await axios.get('https://example.com/api/data');
+
+//     // Respond with the data from the external API
+//     res.json(response.data);
+//   } catch (error) {
+//     // Handle errors
+//     res.status(500).json({ error: 'Internal Server Error' });
+//   }
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
